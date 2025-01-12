9c3b8dc6ed81e5a1c4ecc1a9459c06d006b7c8f4
AnnaSilverIT <student291297@gmail.com>
2025-01-12 15:15:54 +0300

    add eventLisstener

--- script.js
+++ script.js
@@ -8,34 +8,41 @@ const agreeCheckbox = form.elements.agreement;
 const userNameError = document.getElementById('nameError');
 const emailError = document.getElementById('emailError');
 const passwordError = document.getElementById('passwordError');
+const termsError = document.getElementById('agreementError')
+
 
 form.addEventListener('submit', function(evt) {
   evt.preventDefault();
   let hasError = false;
 
+  userNameError.style.display = 'none';
+  emailError.style.display = 'none';
+  termsError.style.display = 'none';
+
   if (userNameInput.value === '') {
     userNameError.textContent = 'Введите имя пользователя.';
     userNameError.style.display = 'block';
     hasError = true;
-    button.disabled = true
   }
 
-  // if (validateEmail(emailInput.value) === false) {
-  //   emailError.textContent = 'Введите корректный email.';
-  //   emailError.style.display = 'block';
-  //   hasError = true;
-  //   button.disabled = true
-  // }
+  if (emailInput.value === '') {
+    emailError.textContent = 'Введите корректный email.';
+    emailError.style.display = 'block';
+    hasError = true;
+  }
 
   if (agreeCheckbox.checked === false) {
     termsError.textContent = 'Необходимо согласие с условиями.';
     termsError.style.display = 'block';
     hasError = true;
-    button.disabled = true
   }
 
   if (hasError === false) {
     alert('Форма успешно отправлена!');
-    button.disabled = false
+    button.removeAttribute('disabled')
+    form.reset()
   }
+ 
 });
+
+    
